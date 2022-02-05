b51d00e26f1087ab62ca2f3f3e7646893261ac11
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2022-02-05 23:07:02 +0900

    nav...

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+    // 네비게이션 설정
+    lateinit var navController :NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
